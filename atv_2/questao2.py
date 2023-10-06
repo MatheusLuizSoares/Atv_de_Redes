50a99ac35b513364a92b0b2f6a9912c9b2f4401a
Matheus Luiz Soares <matheus.luiz1@academico.ifrn.edu.br>
2023-10-06 20:21:55 -0300

    repetição

---
+++ atv_2/questao2.py
@@ -0,0 +1,18 @@
+
+res=1
+n=int(input("informe um número:"))
+while n>1:
+ fatorial= res<=n
+if res<=n:
+ print(f"os números são: {fatorial} ")
+
+else:
+    print("O numero passou")
+
+
+
+
+
+
+
+ 
---
+++ atv_2/questão9.py
@@ -0,0 +1,11 @@
+while True:
+    n=int(input("informe um número"))
+    if n==0: 
+        break
+    if n%2==0:
+        print("par")
+
+    else:
+         print("impar")
+
+    print('fim')
