54793fa059caab77190052149ea398ea5477e6ef
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2023-10-17 19:50:23 +0000

    recorringindo erro

--- atv_1/questão_1_ate_10/questão2.py
+++ atv_1/questão_1_ate_10/questão2.py
@@ -1,5 +1,12 @@
-a=float(input(""))
-b=float(input(""))
-c=float(input(""))
 
-x= -b
+a = float(input("Digite o valor de a:"))
+b = float(input("Digite o valor de b:"))
+c = float(input("Digite o valor de c:"))
+
+delta = (b**2) - 4*a*c
+if delta >= 0:
+    var1 = (-b + delta**0.5) / (2*a)
+    var2 = (-b - delta**0.5) / (2*a)
+    print(f"O valor da  variavel um é {var1:.2f} e o valor da variavel dois é {var2:.2f}")
+else:
+    print("A equação não possui raízes reais.")
--- atv_1/questão_1_ate_10/questão5.py
+++ atv_1/questão_1_ate_10/questão5.py
@@ -1,3 +1,33 @@
 #Escreva um programa que leia um inteiro entre 1 e 12 e imprima o o nome do mês correspondente a este numero. Isto é, se for informado 1 exibir Janeiro, se for informado 2, exibir Fevereiro e assim por diante.
 
 
+numero=int(input("Digite um número de 1 até 12:"))
+if numero==1:
+  print("Janeiro")
+elif numero==2:
+  print("Fevereiro")
+  
+elif numero==3:
+  print("Março")
+elif numero==4:
+  print("Abril")
+elif numero==5:
+  print("Maio")
+elif numero==6:
+    print("Junho")
+elif numero==7:
+ print("Julho")
+elif numero==8:
+  print("Agosto")
+elif numero==9:
+  print("Setembro")
+elif numero==10:
+  print("Outubro")
+elif numero==11:
+   print("Novembro")
+elif numero==12:
+  print("Desembro")
+
+
+
+
